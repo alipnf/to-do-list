78f03802eabece5c39c4bf7d1f409ae26eaf3b19
alipnf <alipnf@proton.me>
2024-03-24 21:55:23 +0700

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
+import Navbar from "./components/navbar";
+
 export default function App() {
-  return (
-    <div className="flex justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <Navbar />;
 }
