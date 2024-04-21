2a0719114791dbe64430541b051acbe75ea0c7af
alipnf <alipnf@proton.me>
2024-04-21 22:09:43 +0700

    feat: Add task list

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import AddTask from "./addTask";
+import TaskList from "./taskList";
 
 export default function Hero() {
+  const [taskList, setTaskList] = useState([]);
+  const addTaskToList = (newTask) => {
+    if (newTask != "") {
+      return setTaskList([...taskList, newTask]);
+    }
+  };
   return (
     <div className="min-h-screen bg-base-200 px-6">
-      <div className="flex justify-center">
-        <AddTask />
+      <div className="flex flex-col justify-center">
+        <AddTask addTaskToList={addTaskToList} />
+        <TaskList list={taskList} />
       </div>
     </div>
   );
