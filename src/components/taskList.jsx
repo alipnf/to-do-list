f640ab813e7f1df1091050e5d30bd27fda8a06a5
alipnf <alipnf@proton.me>
2024-04-21 22:45:10 +0700

    feat: add Strikethrough text

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function TaskList({ list }) {
+  const [checkedItems, setCheckedItems] = useState({});
+
+  const handleCheckboxChange = (index) => {
+    setCheckedItems({ ...checkedItems, [index]: !checkedItems[index] });
+  };
+
   return (
     <div className="mt-4">
       <ul>
         {list.map((item, index) => (
           <li key={index} className="form-control">
             <label className="label flex cursor-pointer justify-start gap-3">
-              <input type="checkbox" className="checkbox" />
-              {item}
+              <input
+                type="checkbox"
+                className="checkbox"
+                checked={checkedItems[index] || false}
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <span
+                style={{
+                  textDecoration: checkedItems[index] ? "line-through" : "none",
+                }}
+              >
+                {item}
+              </span>
             </label>
           </li>
         ))}
