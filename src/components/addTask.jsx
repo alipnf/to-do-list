47181520c48f8d4c841e1e49f764e61530d251a4
alipnf <alipnf@proton.me>
2024-04-21 22:09:04 +0700

    feat: implement add task functionality

--- src/components/addTask.jsx
+++ src/components/addTask.jsx
@@ -1,28 +1,50 @@
-export default function AddTask() {
+import { useState } from "react";
+
+export default function AddTask({ addTaskToList }) {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTaskToList(task);
+    setTask("");
+    console.log(task);
+  };
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
   return (
-    <form className="mt-4 flex items-center gap-3 md:mt-5 lg:mt-8">
-      <input
-        type="text"
-        placeholder="your task"
-        className="input input-sm input-bordered input-primary w-full max-w-xl md:input-md xl:input-lg"
-      />
-      <button
-        className="btn btn-primary btn-sm md:btn-md xl:btn-lg"
-        type="submit"
+    <div>
+      <form
+        className="mt-4 flex w-full items-center justify-center gap-3 md:mt-5 lg:mt-8"
+        onSubmit={handleSubmit}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1.2rem"
-          height="1.2rem"
-          viewBox="0 0 16 16"
+        <input
+          value={task}
+          type="text"
+          placeholder="your task"
+          className="input input-sm input-bordered input-primary w-full max-w-xl md:input-md xl:input-lg"
+          onChange={handleChange}
+        />
+        <button
+          className="btn btn-primary btn-sm md:btn-md xl:btn-lg"
+          type="submit"
         >
-          <path
-            fill="currentColor"
-            d="M15.5 6H10V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V6H.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5H6v5.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V10h5.5a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5"
-          />
-        </svg>
-        Add task
-      </button>
-    </form>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.2rem"
+            height="1.2rem"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="currentColor"
+              d="M15.5 6H10V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V6H.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5H6v5.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V10h5.5a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5"
+            />
+          </svg>
+          Add task
+        </button>
+      </form>
+    </div>
   );
 }
