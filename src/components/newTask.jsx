31c5292ad956054a10afab275fe34774dd3747ea
alipnf <alipnf@proton.me>
2024-04-21 17:02:25 +0700

    feat: add newTask.jsx

---
+++ src/components/newTask.jsx
@@ -0,0 +1,25 @@
+export default function NewTask() {
+  return (
+    <div className="mt-3 flex items-center gap-3">
+      <input
+        type="text"
+        placeholder="your task"
+        className="input input-bordered input-primary w-full max-w-xs"
+      />
+      <button className="btn btn-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1.2rem"
+          height="1.2rem"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="currentColor"
+            d="M15.5 6H10V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V6H.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5H6v5.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V10h5.5a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5"
+          />
+        </svg>
+        New task
+      </button>
+    </div>
+  );
+}
