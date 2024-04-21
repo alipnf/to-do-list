d793233a5da4faf41cc7507d8b8d7f0df9419481
alipnf <alipnf@proton.me>
2024-04-21 17:09:06 +0700

    fix newTask

--- src/components/newTask.jsx
+++ src/components/newTask.jsx
@@ -1,12 +1,12 @@
 export default function NewTask() {
   return (
-    <div className="mt-3 flex items-center gap-3">
+    <form className="mt-3 flex items-center gap-3">
       <input
         type="text"
         placeholder="your task"
         className="input input-bordered input-primary w-full max-w-xs"
       />
-      <button className="btn btn-primary">
+      <button className="btn btn-primary" type="submit">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="1.2rem"
@@ -18,8 +18,8 @@ export default function NewTask() {
             d="M15.5 6H10V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V6H.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5H6v5.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V10h5.5a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5"
           />
         </svg>
-        New task
+        Add task
       </button>
-    </div>
+    </form>
   );
 }
