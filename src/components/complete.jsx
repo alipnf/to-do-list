1c6bf2b105925245cc55607fec6696b8497f6dfe
alipnf <alipnf@proton.me>
2024-07-27 23:51:15 +0700

    refactor: optimize state management

--- src/components/complete.jsx
+++ src/components/complete.jsx
@@ -1,19 +1,11 @@
-import { useState, useEffect } from "react";
-
 export default function Complete({ list }) {
-  const [completeList, setCompleteList] = useState(0);
-
-  useEffect(() => {
-    const completeCount = list.filter(
-      (item) => item.status === "complete",
-    ).length;
-    setCompleteList(completeCount);
-  }, [list]);
-
+  const completeCount = list.filter(
+    (item) => item.status === "complete",
+  ).length;
   return (
     <div className="mt-4 lg:mt-6 xl:mt-8">
       <h1 className="mb-5 text-center text-lg font-bold md:text-base xl:text-xl">
-        Complete ({completeList} / {list.length})
+        Complete ( {completeCount} / {list.length})
       </h1>
     </div>
   );
